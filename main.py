309593a87213d467e75a8fe283b8869961be4dd0
JuanFernandoNina <juanfernandoninac@gmail.com>
2026-09-18 11:30:30 -0400

    Nuevo Text soy fernando

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from nicegui import ui
 
 ui.label('¡Hola, NiceGUI y Git!').classes('text-2xl font-bold text-blue-600')
+ui.label('soy fernando').classes('text-2xl font-bold text-blue-600')
 
 ui.button('Hacer Clic', on_click=lambda: ui.notify('¡Hola desde NiceGUI!'))
 
